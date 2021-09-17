d85c65024f53f98fec396b3c70353992f11be940
Scienceyzac <scienceyzac@gmail.com>
2021-09-17 19:56:07 +1000

    renamed i and b to x and y to make more sense

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let length = { x: 5, y: 5 }; // length and hight for the array.
 
 let dots = []; // the array where we will be keeping the different dots, in the future i will be also adding a previouse dot into it so that it can connect to form a line.
-for (let i = 0; i < length.x; i++) {
+for (let x = 0; x < length.x; x++) {
 	let otherArray = []; // the y array, we dont want one dimension.. or do we?
-	for (let b = 0; b < length.y; b++) {
-		otherArray[b] = { x: i, y: b };
+	for (let y = 0; y < length.y; y++) {
+		otherArray[y] = { x: x, y: y, active: false };
 	}
-	dots[i] = otherArray; // add the y array to the x array.
+	dots[x] = otherArray; // add the y array to the x array.
 }
 console.log(dots);
